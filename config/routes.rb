d789e203a793fca97020d094dda7456fda137d8e
Martin Svalin <martin@lite.nu>
2012-05-12 14:24:04 +0200

    No format in reports routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Trackr::Application.routes.draw do
-  resources :reports, :only => [:create, :show]
+  resources :reports, :only => [:create, :show], :format => false
   root :to => 'signups#index'
 end
