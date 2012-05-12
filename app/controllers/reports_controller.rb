fe534f56ffc125e63f9a6e9a21565bfc90c83a13
Richard Johansson <richard@newsdesk.se>
2012-05-12 16:52:23 +0200

    Add reports/history action to return data since specified time

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,6 +6,11 @@ class ReportsController < ApplicationController
     return render(nothing: true, status: 404) unless @report.exist?
   end
 
+  def history
+    @report = Report.new(params[:report_id])
+    render json: @report.history.select{ |h| h.first >= params[:since].to_i }
+  end
+
   def create
     redirect_to report_path(params[:domain])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Trackr::Application.routes.draw do
-  resources :reports, only: [:create, :show], id: /[^\/]+/, format: false
+  resources :reports, only: [:create, :show], id: /[^\/]+/, format: false do
+    get 'history' => 'reports#history', as: :history
+  end
   root to: 'signups#index'
 end
