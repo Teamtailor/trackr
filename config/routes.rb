86cb39fdffa673e95e475a5d413e0bd6f2a44cae
Martin Svalin <martin@lite.nu>
2012-05-12 14:56:44 +0200

    ruby19 hash syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Trackr::Application.routes.draw do
   post '/reports' => 'reports#create'
-  get  '/reports/:domain' => 'reports#show', :domain => /.*/, :format => false
-  root :to => 'signups#index'
+  get  '/reports/:domain' => 'reports#show', domain: /.*/, format: false
+  root to: 'signups#index'
 end
