9594307e7cbca0fa4d5e218f1b31168f5ffa83aa
Martin Svalin <martin@lite.nu>
2012-05-12 11:54:06 +0200

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trackr::Application.config.secret_token = 'a9c72ebec2577d030de46fb8a19b92ffc98e7cef52f66cd802caf7fb9873d4b853b9c562c1fccb183c542a5964fbea2e7c75868525f3551b88929a1f318d1b52'
